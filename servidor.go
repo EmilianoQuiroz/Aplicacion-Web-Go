f2f403a0e5127d1e24f36a2a89066ee740123b9e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-03-30 21:27:11 -0300

    peticiones al servidor

--- servidor.go
+++ servidor.go
@@ -6,13 +6,24 @@ import (
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Mi pagina principal")
 	})
-	http.HandleFunc("/Productos", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/Productos", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Pagina productos")
 	})
-
+	// Redireccionamiento
+	http.HandleFunc("/Categorias", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/", 301)
+	})
+	// Not found error 404
+	http.HandleFunc("/Noencontro", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	})
+	// Error
+	http.HandleFunc("/Error", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Error en el servidor", 500)
+	})
 	http.ListenAndServe(":8000", nil)
 }
